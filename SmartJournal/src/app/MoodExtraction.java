0dbe2bd8598463babed076c90b54fc5bb728b6e7
CHENYUHAN <25065764@siswa.um.edu.my>
2026-01-10 14:40:57 +0800

    feat: mood (#4)

--- SmartJournal/src/app/MoodExtraction.java
+++ SmartJournal/src/app/MoodExtraction.java
@@ -2,6 +2,15 @@
 
 package app;
 
-public class MoodExtraction {
-    
+public class MoodExtraction{
+    public String extractSummaryForecast(String jsonResponse) {
+        String searchKey = "\"label\":\"";
+        int startIndex = jsonResponse.indexOf(searchKey) + searchKey.length();
+        if (startIndex == -1) {
+            return "Mood data not found";
+        }
+        startIndex= startIndex + searchKey.length();
+        int endIndex = jsonResponse.indexOf("\"", startIndex);
+        return jsonResponse.substring(startIndex, endIndex);
+    }
 }
